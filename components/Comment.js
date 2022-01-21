4485c2868489ca586eeffbf7627c2e2cbdb04f71
FilipStaaf <69985953+FilipStaaf@users.noreply.github.com>
2022-01-21 14:11:07 +0100

    like or unlike comments of other's threads

--- components/Comment.js
+++ components/Comment.js
@@ -1,7 +1,47 @@
 import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon } from "@heroicons/react/outline";
 import Moment from "react-moment";
+import {
+  HeartIcon as HeartIconFilled,
+  ChatIcon as ChatIconFilled,
+} from "@heroicons/react/solid";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { db } from "../firebase";
+import { collection, deleteDoc, doc, onSnapshot, setDoc } from "firebase/firestore";
 
 function Comment({ id, comment }) {
+  const { data: session } = useSession();
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [db, id]
+  );
+  
+  useEffect(
+    () =>
+      setLiked(
+        likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+      ),
+    [likes]
+  );
+
+  const likePost = async () => {
+    if (liked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+    } else {
+      //a collection of likes is created here as the likes-State gains an index length when the heart icon are clicked on
+      await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+        username: session.user.name,
+      });
+    }
+  };
+
+
   return (
     <div className="p-3 flex cursor-pointer border-b border-gray-700">
       <img
@@ -37,13 +77,43 @@ function Comment({ id, comment }) {
             <ChatIcon className="h-5 group-hover:text-[#1d9bf0]" />
           </div>
 
+
+          <div
+            className="flex items-center space-x-1 group"
+            onClick={(e) => {
+              e.stopPropagation();
+              likePost();
+            }}
+          >
+            <div className="icon group-hover:bg-pink-600/10">
+              {liked ? (
+                <HeartIconFilled className="h-5 text-pink-600" />
+              ) : (
+                <HeartIcon className="h-5 group-hover:text-pink-600" />
+              )}
+            </div>
+            {likes.length > 0 && (
+              <span
+                className={`group-hover:text-pink-600 text-sm ${
+                  liked && "text-pink-600"
+                }`}
+              >
+                {likes.length}
+              </span>
+            )}
+          </div>
+
+
+
+
+{/* 
           <div className="flex items-center space-x-1 group">
             <div className="icon group-hover:bg-pink-600/10">
               <HeartIcon className="h-5 group-hover:text-pink-600" />
             </div>
             <span className="group-hover:text-pink-600 text-sm"></span>
           </div>
-
+ */}
           <div className="icon group">
             <ShareIcon className="h-5 group-hover:text-[#1d9bf0]" />
           </div>
